2e56c23f4ca1feec0e15b84cbcbbe12491db213c
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2026-07-02 07:25:39 +0700

    feat: Expand mock product data in ProductDataSource
    Enhance the mock data provider for products to support UI testing and development, including:
    - Update `getProducts()` to include an additional mock product entry (Coca Cola)
    - Replace hardcoded image resource IDs with `R.drawable.sample_product` references
    - Reformat product object instantiation for improved code readability

--- app/src/main/java/com/duyphn/rmsys/data/datasource/ProductDataSource.java
+++ app/src/main/java/com/duyphn/rmsys/data/datasource/ProductDataSource.java
@@ -1,5 +1,6 @@
 package com.duyphn.rmsys.data.datasource;
 
+import com.duyphn.rmsys.R;
 import com.duyphn.rmsys.model.Product;
 
 import java.util.ArrayList;
@@ -10,7 +11,22 @@ public class ProductDataSource {
     public List<Product> getProducts() throws Exception {
         // TODO: Viết code đọc SQLite hoặc gọi API thực tế ở đây
         List<Product> mockList = new ArrayList<>();
-        mockList.add(new Product("Sữa tươi", "cat1", 15000, 10000, 50, "Hộp", 1));
+        mockList.add(new Product(
+                "Sữa tươi",
+                "cat1",
+                15000,
+                10000,
+                50,
+                "Hộp",
+                R.drawable.sample_product));
+        mockList.add(new Product(
+                "Coca Cola 330ml",
+                "cat1",
+                15000,
+                10000,
+                50,
+                "Lon",
+                R.drawable.sample_product));
         return mockList;
     }
 
