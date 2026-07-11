d51ef7dae1e5e63be8925e770abf96c9d35036d6
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2026-07-11 09:32:41 +0700

    feat: Implement ProductDAO for product data management
    Introduce the `ProductDAO` class to handle SQLite database operations for the product entity:
    - Added `insert` method to persist `Product` objects using `ContentValues`
    - Implemented query methods including `findById`, `findByName`, `findByCategoryId`, and `findByActiveState`
    - Integrated standard SQLite `Cursor` processing to map database rows to `Product` model instances
    - Utilized `DatabaseHelper` for managing readable and writable database connections

---
+++ app/src/main/java/com/duyphn/rmsys/data/datasource/ProductDAO.java
@@ -0,0 +1,196 @@
+package com.duyphn.rmsys.data.datasource;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.duyphn.rmsys.model.Product;
+
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+    private final DatabaseHelper dbHelper;
+
+    public ProductDAO(DatabaseHelper dbHelper) {
+        this.dbHelper = dbHelper;
+    }
+
+    public boolean insert(Product product) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COL_PROD_ID, product.getId());
+        values.put(DatabaseHelper.COL_PROD_NAME, product.getName());
+        values.put(DatabaseHelper.COL_PROD_CAT_ID, product.getCategoryId());
+        values.put(DatabaseHelper.COL_PROD_SELL_PRICE, product.getSellingPrice());
+        values.put(DatabaseHelper.COL_PROD_IMP_PRICE, product.getImportPrice());
+        values.put(DatabaseHelper.COL_PROD_STOCK, product.getStock());
+        values.put(DatabaseHelper.COL_PROD_UNIT, product.getUnit());
+        values.put(DatabaseHelper.COL_PROD_IMAGE_RES_ID, product.getImageResId());
+        values.put(DatabaseHelper.COL_PROD_DESCRIPTION, product.getDescription());
+
+        long result = db.insert("product", null, values);
+        db.close();
+        return result != -1;
+    }
+
+    public List<Product> findByActiveState(boolean active) {
+        List<Product> productList = new ArrayList<>();
+        String selectQuery =
+            "SELECT " +
+                DatabaseHelper.COL_PROD_ID + ", " +
+                DatabaseHelper.COL_PROD_NAME + ", " +
+                DatabaseHelper.COL_PROD_CAT_ID + ", " +
+                DatabaseHelper.COL_PROD_SELL_PRICE + ", " +
+                DatabaseHelper.COL_PROD_IMP_PRICE + ", " +
+                DatabaseHelper.COL_PROD_STOCK + ", " +
+                DatabaseHelper.COL_PROD_UNIT + ", " +
+                DatabaseHelper.COL_PROD_IMAGE_RES_ID + ", " +
+                DatabaseHelper.COL_PROD_DESCRIPTION + " " +
+            "FROM " + DatabaseHelper.TABLE_PRODUCT + " " +
+            "WHERE " + DatabaseHelper.COL_PROD_ACTIVE + "= ?";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String activeValue = active ? "1" : "0";
+        Cursor cursor = db.rawQuery(selectQuery, new String[]{ activeValue });
+
+        if (cursor.moveToFirst()) {
+            do {
+                Product product = new Product();
+                product.setId(cursor.getString(0));
+                product.setName(cursor.getString(1));
+                product.setCategoryId(cursor.getString(2));
+                product.setSellingPrice(cursor.getDouble(3));
+                product.setImportPrice(cursor.getDouble(4));
+                product.setStock(cursor.getInt(5));
+                product.setUnit(cursor.getString(6));
+                product.setImageResId(cursor.getInt(7));
+                product.setDescription(cursor.getString(8));
+                productList.add(product);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return productList;
+    }
+
+    public Product findById(String id) {
+        Product product = new Product();
+        String selectQuery =
+                "SELECT " +
+                        DatabaseHelper.COL_PROD_ID + ", " +
+                        DatabaseHelper.COL_PROD_NAME + ", " +
+                        DatabaseHelper.COL_PROD_CAT_ID + ", " +
+                        DatabaseHelper.COL_PROD_SELL_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_IMP_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_STOCK + ", " +
+                        DatabaseHelper.COL_PROD_UNIT + ", " +
+                        DatabaseHelper.COL_PROD_IMAGE_RES_ID + ", " +
+                        DatabaseHelper.COL_PROD_DESCRIPTION + " " +
+                "FROM " + DatabaseHelper.TABLE_PRODUCT + " " +
+                "WHERE " + DatabaseHelper.COL_PROD_ID + "= ?";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, new String[] { id });
+
+        if (cursor.moveToFirst()) {
+            do {
+                product.setId(cursor.getString(0));
+                product.setName(cursor.getString(1));
+                product.setCategoryId(cursor.getString(2));
+                product.setSellingPrice(cursor.getDouble(3));
+                product.setImportPrice(cursor.getDouble(4));
+                product.setStock(cursor.getInt(5));
+                product.setUnit(cursor.getString(6));
+                product.setImageResId(cursor.getInt(7));
+                product.setDescription(cursor.getString(8));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return product;
+    }
+
+    public List<Product> findByName(String name) {
+        List<Product> productList = new ArrayList<>();
+        String selectQuery =
+                "SELECT " +
+                        DatabaseHelper.COL_PROD_ID + ", " +
+                        DatabaseHelper.COL_PROD_NAME + ", " +
+                        DatabaseHelper.COL_PROD_CAT_ID + ", " +
+                        DatabaseHelper.COL_PROD_SELL_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_IMP_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_STOCK + ", " +
+                        DatabaseHelper.COL_PROD_UNIT + ", " +
+                        DatabaseHelper.COL_PROD_IMAGE_RES_ID + ", " +
+                        DatabaseHelper.COL_PROD_DESCRIPTION + " " +
+                "FROM " + DatabaseHelper.TABLE_PRODUCT + " " +
+                "WHERE " + DatabaseHelper.COL_PROD_ID + "= '" + name + "'";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Product product = new Product();
+                product.setId(cursor.getString(0));
+                product.setName(cursor.getString(1));
+                product.setCategoryId(cursor.getString(2));
+                product.setSellingPrice(cursor.getDouble(3));
+                product.setImportPrice(cursor.getDouble(4));
+                product.setStock(cursor.getInt(5));
+                product.setUnit(cursor.getString(6));
+                product.setImageResId(cursor.getInt(7));
+                product.setDescription(cursor.getString(8));
+                productList.add(product);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return productList;
+    }
+
+    public List<Product> findByCategoryId(String cat_id) {
+        List<Product> productList = new ArrayList<>();
+        String selectQuery =
+                "SELECT " +
+                        DatabaseHelper.COL_PROD_ID + ", " +
+                        DatabaseHelper.COL_PROD_NAME + ", " +
+                        DatabaseHelper.COL_PROD_CAT_ID + ", " +
+                        DatabaseHelper.COL_PROD_SELL_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_IMP_PRICE + ", " +
+                        DatabaseHelper.COL_PROD_STOCK + ", " +
+                        DatabaseHelper.COL_PROD_UNIT + ", " +
+                        DatabaseHelper.COL_PROD_IMAGE_RES_ID + ", " +
+                        DatabaseHelper.COL_PROD_DESCRIPTION + " " +
+                "FROM " + DatabaseHelper.TABLE_PRODUCT + " " +
+                "WHERE " + DatabaseHelper.COL_PROD_ID + "= '" + cat_id + "'";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Product product = new Product();
+                product.setId(cursor.getString(0));
+                product.setName(cursor.getString(1));
+                product.setCategoryId(cursor.getString(2));
+                product.setSellingPrice(cursor.getDouble(3));
+                product.setImportPrice(cursor.getDouble(4));
+                product.setStock(cursor.getInt(5));
+                product.setUnit(cursor.getString(6));
+                product.setImageResId(cursor.getInt(7));
+                product.setDescription(cursor.getString(8));
+                productList.add(product);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return productList;
+    }
+
+
+
+
+}
